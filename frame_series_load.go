1f295c67b71f96f906b60737bef975a635f28efa
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-06-28 15:56:57 +0300

    Added LoadFrameSeries

---
+++ frame_series_load.go
@@ -0,0 +1,58 @@
+package hasp
+
+import (
+	"image"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+
+	"github.com/rmcsoft/chanim"
+)
+
+func loadFrames(path string) ([]chanim.Frame, error) {
+	ppixmapFiles, err := filepath.Glob(filepath.Join(path, "*.ppixmap"))
+	if err != nil {
+		return nil, err
+	}
+	sort.Strings(ppixmapFiles)
+
+	frames := make([]chanim.Frame, 0)
+	for _, ppixmapFile := range ppixmapFiles {
+		ppixmap, err := chanim.LoadPackedPixmap(ppixmapFile)
+		if err != nil {
+			return nil, err
+		}
+
+		drawOperation := chanim.NewDrawPackedPixmapOperation(image.Point{0, 0}, ppixmap)
+		frames = append(frames, chanim.Frame{
+			DrawOperations: []chanim.DrawOperation{drawOperation},
+		})
+	}
+
+	return frames, nil
+}
+
+// LoadFrameSeries loads frame series
+// It is assumed that each series is located in its own subdirectory.
+// The name of the series is the same as the subdirectory name.
+func LoadFrameSeries(path string) ([]chanim.FrameSeries, error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	allFrameSeries := make([]chanim.FrameSeries, 0)
+	for _, fileInfo := range files {
+		if fileInfo.IsDir() {
+			frameSeriesName := fileInfo.Name()
+			frames, err := loadFrames(filepath.Join(path, fileInfo.Name()))
+			if err != nil {
+				return nil, err
+			}
+
+			allFrameSeries = append(allFrameSeries, chanim.FrameSeries{frameSeriesName, frames})
+		}
+	}
+
+	return allFrameSeries, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rmcsoft/hasp
 
 go 1.12
+
+require github.com/rmcsoft/chanim v0.0.0-20190628081211-4ff01330a927
