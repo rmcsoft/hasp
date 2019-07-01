523f6ab9cb5af6d1437829ed546c92cd75016414
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-07-01 17:07:28 +0300

    Added state definition

---
+++ state.go
@@ -0,0 +1,13 @@
+package hasp
+
+// State defines an interface for states
+type State interface {
+	Enter(event Event) (EventSources, error)
+	Leave(event Event) bool
+
+	GetAnimation() string
+	GetSound() []uint16
+}
+
+// States is set of states.
+type States = map[string]State
