5c471e630a7049b244d90af090298e0067dfdc87
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-07-03 18:39:37 +0300

    Added example for HotWordDetector

---
+++ examples/hot_word_detector/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rmcsoft/hasp"
+)
+
+func main() {
+	capDev := os.Args[1]
+	modelPath := os.Args[2]
+	keywordPath := os.Args[3]
+
+	hotWordDetector, err := hasp.NewHotWordDetector(capDev, modelPath, keywordPath)
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		time.Sleep(5 * time.Minute)
+		hotWordDetector.Close()
+	}()
+
+	for event := range hotWordDetector.Events() {
+		v, _ := event.GetVoice()
+		fmt.Printf("Voice=%v\n", v)
+	}
+
+	fmt.Printf("HotWordDetector was closed\n")
+}
