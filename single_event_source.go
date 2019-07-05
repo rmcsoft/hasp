21f8385ea6923b333339495f886de714238fab7c
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-07-05 17:02:36 +0300

    Added SingleEventSource

---
+++ single_event_source.go
@@ -0,0 +1,31 @@
+package hasp
+
+type singleEventSource struct {
+	name      string
+	eventChan chan *Event
+}
+
+// NewSingleEventSource creates new singleEventSource
+func NewSingleEventSource(name string, fn func() *Event) EventSource {
+	es := &singleEventSource{
+		name:      name,
+		eventChan: make(chan *Event),
+	}
+
+	go func() {
+		es.eventChan <- fn()
+	}()
+
+	return es
+}
+
+func (es *singleEventSource) Name() string {
+	return es.name
+}
+
+func (es *singleEventSource) Events() chan *Event {
+	return es.eventChan
+}
+
+func (es *singleEventSource) Close() {
+}
