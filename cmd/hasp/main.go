dd11f29b6f395e90581085867ad77fd09b2c4e50
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-07-11 09:06:58 +0300

    Small improvements

--- cmd/hasp/main.go
+++ cmd/hasp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"time"
@@ -16,30 +17,35 @@ const (
 )
 
 type options struct {
-	PackedImageDir string `short:"i" long:"image-dir" description:"Packed image directory needed for animation"`
+	PackedImageDir string `short:"i" long:"image-dir" description:"Packed image directory needed for animation" required:"true"`
 	UseSDL         bool   `short:"s" long:"use-sdl"   description:"Render with sdl"`
 
 	CaptureDevice  string `short:"c" long:"capture-dev" default:"hw:0" description:"Sound capture device name"`
 	PlayDevice     string `short:"p" long:"play-dev"    default:"mono" description:"Sound play device name"`
-	ModelParamPath string `short:"m" long:"model-param" description:"Path to file containing model parameters"`
-	KeywordPath    string `short:"k" long:"keyword"     description:"Path to keyword file"`
+	ModelParamPath string `short:"m" long:"model-param" description:"Path to file containing model parameters" required:"true"`
+	KeywordPath    string `short:"k" long:"keyword"     description:"Path to keyword file" required:"true"`
+}
+
+func fail(err error) {
+	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	os.Exit(1)
 }
 
 func parseCmd() options {
 	var opts options
-	var cmdParser = flags.NewParser(&opts, flags.Default)
+	var cmdParser = flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash)
 	var err error
 
 	if _, err = cmdParser.Parse(); err != nil {
 		if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+			fmt.Println(flagsErr)
 			os.Exit(0)
-		} else {
-			panic(err)
 		}
+		fail(err)
 	}
 
 	if opts.PackedImageDir, err = filepath.Abs(opts.PackedImageDir); err != nil {
-		panic(err)
+		fail(err)
 	}
 
 	return opts
@@ -54,7 +60,7 @@ func makePaintEngine(opts options) chanim.PaintEngine {
 		paintEngine, err = chanim.NewKMSDRMPaintEngine(0, pixFormat)
 	}
 	if err != nil {
-		panic(err)
+		fail(err)
 	}
 
 	return paintEngine
@@ -64,7 +70,7 @@ func makeAnimator(opts options) *chanim.Animator {
 	paintEngine := makePaintEngine(opts)
 	animator, err := hasp.CreateAnimator(paintEngine, opts.PackedImageDir)
 	if err != nil {
-		panic(err)
+		fail(err)
 	}
 	return animator
 }
@@ -72,7 +78,7 @@ func makeAnimator(opts options) *chanim.Animator {
 func makeHotWordDetector(opts options) hasp.EventSource {
 	hotWordDetector, err := hasp.NewHotWordDetector(opts.CaptureDevice, opts.ModelParamPath, opts.KeywordPath)
 	if err != nil {
-		panic(err)
+		fail(err)
 	}
 	return hotWordDetector
 }
@@ -80,7 +86,7 @@ func makeHotWordDetector(opts options) hasp.EventSource {
 func makeSoundPlayer(opts options) *hasp.SoundPlayer {
 	player, err := hasp.NewSoundPlayer(opts.PlayDevice, 16000)
 	if err != nil {
-		panic(err)
+		fail(err)
 	}
 	return player
 }
@@ -100,7 +106,7 @@ func makeCharacter(opts options) *hasp.Character {
 	soundPlayer := makeSoundPlayer(opts)
 	character, err := hasp.NewCharacter("idle", states, nil, eventSources, animator, soundPlayer)
 	if err != nil {
-		panic(err)
+		fail(err)
 	}
 
 	return character
@@ -112,6 +118,6 @@ func main() {
 	character := makeCharacter(opts)
 	err := character.Run()
 	if err != nil {
-		panic(err)
+		fail(err)
 	}
 }
